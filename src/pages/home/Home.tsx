e9cd3bd910f8537e2be53578e64e415e9f457726
Behruzbek <behruzbekodilov0724@gmail.com>
2025-03-05 21:05:21 +0500

    home tsx qoshildi va home.css qoshildi

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import "./Home.css"; 
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <h1>Welcome to Our App</h1>
+      <p>Iltimos, tizimga kiring yoki ro‘yxatdan o‘ting.</p>
+
+      <div className="button-group">
+        <button onClick={() => navigate("/login")} className="login-btn">
+          Login
+        </button>
+        <button onClick={() => navigate("/register")} className="register-btn">
+          Register
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
